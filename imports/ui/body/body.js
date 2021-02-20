f80f5d77255b5ba0a45f31bb671f5e0f5a148210
Mihail Marian <mihailmarian12@gmail.com>
2021-02-20 16:24:06 +0000

    form working

--- imports/ui/body/body.js
+++ imports/ui/body/body.js
@@ -9,3 +9,16 @@ Template.body.helpers({
     return Posts.find({}, { sort: { createdAt: -1 } });
   },
 });
+
+Template.body.events({
+  "submit .new-post"(event) {
+    event.preventDefault();
+
+    const currentText = event.target.textarea_post.value;
+
+    Posts.insert({
+      text: currentText,
+      createdAt: new Date(),
+    });
+  },
+});
