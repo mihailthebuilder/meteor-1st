b279ea55bcb7924c3721aac27639e2d1ceb15eac
Mihail Marian <mihailmarian12@gmail.com>
2021-02-23 12:04:58 +0000

    can filter for own posts

--- imports/ui/body/body.js
+++ imports/ui/body/body.js
@@ -1,18 +1,27 @@
 import { Template } from "meteor/templating";
 import { Posts } from "../../api/content.js";
 import { Meteor } from "meteor/meteor";
+import { ReactiveDict } from "meteor/reactive-dict";
 
 import "../post/post.js";
 import "./body.html";
 import "./body.scss";
 
 Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+  this.state.set("onlyOwnPosts", false);
   Meteor.subscribe("posts");
 });
 
 Template.body.helpers({
   posts() {
-    return Posts.find({}, { sort: { createdAt: -1 } });
+    const instance = Template.instance();
+    const sortMethod = { sort: { createdAt: -1 } };
+
+    if (instance.state.get("onlyOwnPosts")) {
+      return Posts.find({ owner: { $eq: Meteor.user()._id } }, sortMethod);
+    }
+    return Posts.find({}, sortMethod);
   },
 });
 
@@ -28,5 +37,8 @@ Template.body.events({
     Meteor.call("content.insert", currentText, contentType, contentRelId);
 
     event.target.textarea_content.value = "";
+  },
+  "change .own-posts input"(event, instance) {
+    instance.state.set("onlyOwnPosts", event.target.checked);
   },
 });
